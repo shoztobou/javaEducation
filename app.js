bfe58b6b7140e8dcb074dacb6189e6be8fcf7e82
shoztobou <yravesel@gmail.com>
2021-05-16 19:38:26 +0300

    second

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-let name = shoztobou
-const firstDictionary = { John Whick: 1, 2, 3; Edward Cullen: 4, 5, 6}
-
-console.log(firstDictionary)
+let name = "shoztobou"
+const firstDictionary = ["John", "Whick", 1, 2, 3, "Johnathan", 4, 5, 6]
+for (i in firstDictionary){
+    console.log(firstDictionary[i])
+}
